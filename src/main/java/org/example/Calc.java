991bca8ac33cb91e75a2b73b48ac283839484e63
tk7580 <tkcode7580@gmail.com>
2025-03-19 13:16:52 +0900

    10 * 20 + 10 + 5 * 2

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -1,15 +1,76 @@
 package org.example;
 
+ import java.util.Arrays;
+ import java.util.stream.Collectors;
+
 public class Calc {
 
     public static int run(String exp) {
+        // 단일항이 들어오면 바로 리턴
+        if (!exp.contains(" ")) {
+            return Integer.parseInt(exp);
+        }
 
-        String[] bits = exp.split(" \\+ ");
+        boolean needToMulti = exp.contains("*");
+        boolean needToPlus = exp.contains("+");
 
-        int a = Integer.parseInt(bits[0]);
-        int b = Integer.parseInt(bits[1]);
+        if (needToCompound) {
+            String[] bits = exp.split(" \\+ ");
 
-        return a + b;
+            return Integer.parseInt(bits[0]) + run(bits[1]);
+            String newExp = Arrays.stream(bits)
+                    .mapToInt(Calc::run)
+                    .mapToObj(e -> e + "")
+                    .collect(Collectors.joining(" + "));
+
+            //            String newExp = "";
+            //            for (int i = 0; i < bits.length; i++) {
+            //                int result = Calc.run(bits[i]);
+            //                newExp += result;
+            //                if (i < bits.length - 1) {
+            //                    newExp += " + ";
+            //                }
+            //            }
+
+
+            //            StringBuilder newExpBuilder = new StringBuilder();
+            //            for (int i = 0; i < bits.length; i++) {
+            //                newExpBuilder.append(Calc.run(bits[i]));
+            //                if (i < bits.length - 1) {
+            //                    newExpBuilder.append(" + ");
+            //                }
+            //            }
+            //            String newExp = newExpBuilder.toString();
+
+
+            return run(newExp);
+        }
+
+        if (needToPlus) {
+            exp = exp.replace("- ", "+ -");
+
+            String[] bits = exp.split(" \\+ ");
+
+            int sum = 0;
+
+            for (int i = 0; i < bits.length; i++) {
+                sum += Integer.parseInt(bits[i]);
+            }
+
+            return sum;
+        } else if (needToMulti) {
+            String[] bits = exp.split(" \\* ");
+
+            int sum = 1;
+
+            for (int i = 0; i < bits.length; i++) {
+                sum *= Integer.parseInt(bits[i]);
+            }
+
+            return sum;
+        }
+
+        throw new RuntimeException("해석 불가 : 올바른 계산식이 아닙니다");
     }
 
 }
--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -7,28 +7,81 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalcTest {
 
-    @Test
-    @DisplayName("1 + 1 == 2")
-    public void test1() {
-        assertThat(Calc.run("1 + 1")).isEqualTo(2);
-    }
+//    @Test
+//    @DisplayName("1 + 1 == 2")
+//    public void test1() {
+//        assertThat(Calc.run("1 + 1")).isEqualTo(2);
+//    }
+//
+//    @Test
+//    @DisplayName("2 + 1 == 3")
+//    public void test2() {
+//        assertThat(Calc.run("2 + 1")).isEqualTo(3);
+//    }
+//
+//    @Test
+//    @DisplayName("2 + 2 == 4")
+//    public void test3() {
+//        assertThat(Calc.run("2 + 2")).isEqualTo(4);
+//    }
+//
+//    @Test
+//    @DisplayName("1000 + 200 == 1200")
+//    public void test4() {
+//        assertThat(Calc.run("1000 + 200")).isEqualTo(1200);
+//    }
+//
+//    @Test
+//    @DisplayName("2 - 1 == 1")
+//    public void test5() {
+//        assertThat(Calc.run("2 - 1")).isEqualTo(1);
+//    }
+//
+//    @Test
+//    @DisplayName("50 - 30 == 20")
+//    public void test6() {
+//        assertThat(Calc.run("50 - 30")).isEqualTo(20);
+//    }
+//
+//    @Test
+//    @DisplayName("7 - 4 == 3")
+//    public void test7() {
+//        assertThat(Calc.run("7 - 4")).isEqualTo(3);
+//    }
+//
+//    @Test
+//    @DisplayName("10 + 20 + 30 == 60")
+//    public void test8() {
+//        assertThat(Calc.run("10 + 20 + 30")).isEqualTo(60);
+//    }
+//
+//    @Test
+//    @DisplayName("10 - 20 + 30 == 20")
+//    public void test10() {
+//        assertThat(Calc.run("10 - 20 + 30")).isEqualTo(20);
+//    }
+//
+//    @Test
+//    @DisplayName("10 + 20 - 30 == 0")
+//    public void test10_2() {
+//        assertThat(Calc.run("10 + 20 - 30")).isEqualTo(0);
+//    }
+
+//    @Test
+//    @DisplayName("10 - 10 - 10 - 10 + 10 + 10 - 10 == -10")
+//    public void test11() {
+//        assertThat(Calc.run("10 - 10 - 10 - 10 + 10 + 10 - 10")).isEqualTo(-10);
+//    }
+
+//    @Test
+//    @DisplayName("10 * 10 * 10 == 1000")
+//    public void test12() {
+//        assertThat(Calc.run("10 * 10 * 10")).isEqualTo(1000);
+//    }
 
     @Test
-    @DisplayName("2 + 1 == 3")
-    public void test2() {
-        assertThat(Calc.run("2 + 1")).isEqualTo(3);
+    @DisplayName("10 * 10 - 10 / 10 * 10 + 10 / 10 * 10 == 100")
+    public void test12() {
+        assertThat(Calc.run("10 * 10 - 10 / 10 * 10 + 10 / 10 * 10")).isEqualTo(100);
     }
-
-    @Test
-    @DisplayName("2 + 2 == 4")
-    public void test3() {
-        assertThat(Calc.run("2 + 2")).isEqualTo(4);
-    }
-
-    @Test
-    @DisplayName("1000 + 200 == 1200")
-    public void test4() {
-        assertThat(Calc.run("1000 + 200")).isEqualTo(1200);
-    }
-
 }
